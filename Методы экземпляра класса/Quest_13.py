ba942d6e689fa7fc374e32cc8975aefbedceb039
DanilXarlamov22 <100994036+DanilXarlamov22@users.noreply.github.com>
2023-10-27 19:55:39 +0300

    Quest(13-14)

--- Методы экземпляра класса/Quest_13.py
+++ Методы экземпляра класса/Quest_13.py
@@ -1,12 +1,32 @@
+from collections import OrderedDict
+
+
 class Postman:
-    def __init__(self, delivery_data=[]):
-        self.delivery_data = delivery_data
+    def __init__(self):
+        self.delivery_data = []
+        self.order_dict = OrderedDict()
 
     def add_delivery(self, street, num, apart):
-        self.delivery_data.append((street, num, apart))
+        delivery = (street, num, apart)
+        if delivery not in self.delivery_data:
+            self.delivery_data.append(delivery)
+            self.order_dict = OrderedDict(((s, n), []) for s, n, _ in self.delivery_data)
+            for s, n, a in self.delivery_data:
+                self.order_dict[(s, n)].append(a)
 
     def get_houses_for_street(self, s):
-        return [b for a, b, _ in self.delivery_data if a == s]
+        return [n for (street, n), _ in self.order_dict.items() if street == s]
 
     def get_flats_for_house(self, s, num):
-        return [c for a, b, c in self.delivery_data if a == s and b == num]
+        return self.order_dict.get((s, num), [])
+
+
+postman = Postman()
+
+postman.add_delivery('Советская', 151, 74)
+postman.add_delivery('Советская', 151, 75)
+postman.add_delivery('Советская', 90, 2)
+postman.add_delivery('Советская', 151, 74)
+
+print(postman.get_houses_for_street('Советская'))
+print(postman.get_flats_for_house('Советская', 151))
